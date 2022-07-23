28ff3f52749c083748f568ffe3cc12415fa1ff27
Alex Nitta <a.j.nitta@gmail.com>
2022-07-23 06:12:02 -0700

    feat: update mac.sh and README.md

--- README.md
+++ README.md
@@ -1,20 +1,22 @@
 # laptop
+
 Tools for setting up a new Mac laptop for development
 
 ## Resources
 
 I've heard other developers talk about how they have a setup script that they use to migrate from one development machine to another. A quick Google search turned up the [thoughtbot/laptop](https://github.com/thoughtbot/laptop) repo. I looked through the [source](https://github.com/thoughtbot/laptop/blob/main/mac) and decided I wanted something slightly different.
 
-## Dotfiles
+## Managing dotfiles
 
 Dotfiles are files like `.bash_profile` that customize your tools. There's a CLI called [`rcm`](https://github.com/thoughtbot/rcm) that allows you to set up a separate folder called `.dotfiles` for managing your dotfiles in a reproducible / migratable way.
 
 If you've already made changes to your own dotfiles and want to move them to a new machine, you can follow [this section](http://thoughtbot.github.io/rcm/rcm.7.html#QUICK_START_FOR_EMPTY_DOTFILES_DIRECTORIES) of the tutorial to set up a `~/.dotfiles` directory and then move specific dotfiles into it. When following this tutorial, I got tripped up because I didn't realize all the commands had to be run from the home directory, i.e. from the `~` directory.
 
 Here are the commands I ran:
+
 ```bash
 # install rcm
-$ brew install rcm 
+$ brew install rcm
 
 # Make sure you're in your home folder that contains your dotfiles
 $ cd ~
@@ -47,3 +49,32 @@ $ env RCRC=/dev/null rcup -B 0 -g > install.sh
 
 Now `~/.dotfiles` folder is a nice bundle of the dotfiles we care about for development purposes that contains its own install script.
 
+## Running the setup script
+
+Now that you've got the `~/.dotfiles` folder set up, you can execute the setup script. The setup script and the rest of this README are largely copied from [https://github.com/thoughtbot/laptop](https://github.com/thoughtbot/laptop).
+
+## Install
+
+Download the script:
+
+```sh
+curl --remote-name https://raw.githubusercontent.com/alexnitta/laptop/main/mac
+```
+
+Review the script (avoid running scripts you haven't read!):
+
+```sh
+less mac
+```
+
+Execute the downloaded script:
+
+```sh
+sh mac 2>&1 | tee ~/laptop.log
+```
+
+Optionally, review the log:
+
+```sh
+less ~/laptop.log
+```
---
+++ mac.sh
@@ -0,0 +1,143 @@
+#!/bin/sh
+
+# Welcome to the laptop script!
+# Be prepared to turn your laptop (or desktop, no haters here)
+# into an awesome development machine.
+
+# shellcheck disable=SC3043
+
+fancy_echo() {
+  local fmt="$1"; shift
+
+  # shellcheck disable=SC2059
+  printf "\\n$fmt\\n" "$@"
+}
+
+append_to_bashrc() {
+  local text="$1" bashrc
+  local skip_new_line="${2:-0}"
+
+  if [ -w "$HOME/.bashrc.local" ]; then
+    bashrc="$HOME/.bashrc.local"
+  else
+    bashrc="$HOME/.bashrc"
+  fi
+
+  if ! grep -Fqs "$text" "$bashrc"; then
+    if [ "$skip_new_line" -eq 1 ]; then
+      printf "%s\\n" "$text" >> "$bashrc"
+    else
+      printf "\\n%s\\n" "$text" >> "$bashrc"
+    fi
+  fi
+}
+
+# shellcheck disable=SC2154
+trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
+
+set -e
+
+if [ ! -d "$HOME/.bin/" ]; then
+  mkdir "$HOME/.bin"
+fi
+
+if [ ! -f "$HOME/.bashrc" ]; then
+  touch "$HOME/.bashrc"
+fi
+
+# # shellcheck disable=SC2016
+# append_to_bashrc 'export PATH="$HOME/.bin:$PATH"'
+
+# Determine Homebrew prefix
+arch="$(uname -m)"
+if [ "$arch" = "arm64" ]; then
+  HOMEBREW_PREFIX="/opt/homebrew"
+else
+  HOMEBREW_PREFIX="/usr/local"
+fi
+
+if ! command -v brew >/dev/null; then
+  fancy_echo "Installing Homebrew ..."
+    /bin/bash -c \
+      "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    append_to_bashrc "eval \"\$($HOMEBREW_PREFIX/bin/brew shellenv)\""
+
+    export PATH="$HOMEBREW_PREFIX/bin:$PATH"
+fi
+
+if brew list | grep -Fq brew-cask; then
+  fancy_echo "Uninstalling old Homebrew-Cask ..."
+  brew uninstall --force brew-cask
+fi
+
+fancy_echo "Updating Homebrew formulae ..."
+brew update --force # https://github.com/Homebrew/brew/issues/1151
+brew bundle --file=- <<EOF
+
+# Unix
+brew "git"
+brew "openssl"
+brew "rcm"
+brew "bash"
+brew "rsync"
+brew "tree"
+
+# GitHub
+brew "gh"
+
+# Image manipulation
+brew "svgo"
+
+# Programming language prerequisites and package managers
+brew "libyaml" # should come after openssl
+brew "coreutils"
+brew "yarn"
+cask "gpg-suite-no-mail"
+
+# Productivity
+brew install --cask fig
+
+EOF
+
+fancy_echo "Configuring asdf version manager ..."
+if [ ! -d "$HOME/.asdf" ]; then
+  brew install asdf
+  append_to_bashrc "source $(brew --prefix asdf)/asdf.sh" 1
+fi
+
+alias install_asdf_plugin=add_or_update_asdf_plugin
+add_or_update_asdf_plugin() {
+  local name="$1"
+  local url="$2"
+
+  if ! asdf plugin-list | grep -Fq "$name"; then
+    asdf plugin-add "$name" "$url"
+  else
+    asdf plugin-update "$name"
+  fi
+}
+
+# shellcheck disable=SC1090
+. "$(brew --prefix asdf)/asdf.sh"
+add_or_update_asdf_plugin "nodejs" "https://github.com/asdf-vm/asdf-nodejs.git"
+
+install_asdf_language() {
+  local language="$1"
+  local version
+  version="$(asdf list-all "$language" | grep -v "[a-z]" | tail -1)"
+
+  if ! asdf list "$language" | grep -Fq "$version"; then
+    asdf install "$language" "$version"
+    asdf global "$language" "$version"
+  fi
+}
+
+fancy_echo "Installing latest Node ..."
+install_asdf_language "nodejs"
+
+if [ -f "$HOME/.laptop.local" ]; then
+  fancy_echo "Running your customizations from ~/.laptop.local ..."
+  # shellcheck disable=SC1090
+  . "$HOME/.laptop.local"
+fi
